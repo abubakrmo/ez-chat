7928f7eee460f8a96ffdfd78ef5c22c6c13d8f5a
abubakrmo <abubakarmufutauoseni@gmail.com>
2024-03-30 16:56:51 +0000

    signup hooked up with authentication

--- src/signup.js
+++ src/signup.js
@@ -1,3 +1,21 @@
+import {initializeApp} from 'firebase/app'
+import {getAuth, onAuthStateChanged, createUserWithEmailAndPassword} from 'firebase/auth' 
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAWhUhO0gg65-WcBHovTdPa3MAQcEorNcU",
+    authDomain: "ezchat-e864e.firebaseapp.com",
+    projectId: "ezchat-e864e",
+    storageBucket: "ezchat-e864e.appspot.com",
+    messagingSenderId: "1030820271963",
+    appId: "1:1030820271963:web:9522162bea79c525552c7e",
+    measurementId: "G-HL9ZR0WSKV"
+  };
+
+initializeApp(firebaseConfig);
+
+
+
+
 const makeVisible = document.querySelector('.passwordVisibility')
 makeVisible.addEventListener('click', () =>{
     const password = document.querySelector('.pwd')
@@ -6,4 +24,25 @@ makeVisible.addEventListener('click', () =>{
     }else{
         password.type = "password"
     }
+})
+
+
+const auth = getAuth()
+const signupform = document.querySelector('.signupform')
+signupform.addEventListener('submit', e=>{
+    e.preventDefault();
+    
+    const email = signupform.useremail.value;
+    const password = signupform.userpassword.value;
+
+   
+    createUserWithEmailAndPassword(auth, email, password)
+        .then(cred =>{
+            console.log(cred.user)
+            alert('User created succesfully')
+            window.open('/dist/login.html')
+        })
+        .catch(err=>{
+            console.log(err.message)
+        })
 })
