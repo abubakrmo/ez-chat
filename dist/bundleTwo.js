2267dcbb33591d4c099bff72c42a6af83a008f87
abubakrmo <abubakarmufutauoseni@gmail.com>
2024-03-30 15:38:01 +0000

    signup js file bundled

---
+++ dist/bundleTwo.js
@@ -0,0 +1,32 @@
+/*
+ * ATTENTION: An "eval-source-map" devtool has been used.
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file with attached SourceMaps in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+/******/ (() => { // webpackBootstrap
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./src/signup.js":
+/*!***********************!*\
+  !*** ./src/signup.js ***!
+  \***********************/
+/***/ (() => {
+
+eval("const makeVisible = document.querySelector('.passwordVisibility')\nmakeVisible.addEventListener('click', () =>{\n    const password = document.querySelector('.pwd')\n    if(password.type === \"password\"){\n        password.type = \"text\";\n    }else{\n        password.type = \"password\"\n    }\n})//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiLi9zcmMvc2lnbnVwLmpzIiwibWFwcGluZ3MiOiJBQUFBO0FBQ0E7QUFDQTtBQUNBO0FBQ0E7QUFDQSxLQUFLO0FBQ0w7QUFDQTtBQUNBLENBQUMiLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly9lemNoYXQvLi9zcmMvc2lnbnVwLmpzPzI2NDAiXSwic291cmNlc0NvbnRlbnQiOlsiY29uc3QgbWFrZVZpc2libGUgPSBkb2N1bWVudC5xdWVyeVNlbGVjdG9yKCcucGFzc3dvcmRWaXNpYmlsaXR5Jylcbm1ha2VWaXNpYmxlLmFkZEV2ZW50TGlzdGVuZXIoJ2NsaWNrJywgKCkgPT57XG4gICAgY29uc3QgcGFzc3dvcmQgPSBkb2N1bWVudC5xdWVyeVNlbGVjdG9yKCcucHdkJylcbiAgICBpZihwYXNzd29yZC50eXBlID09PSBcInBhc3N3b3JkXCIpe1xuICAgICAgICBwYXNzd29yZC50eXBlID0gXCJ0ZXh0XCI7XG4gICAgfWVsc2V7XG4gICAgICAgIHBhc3N3b3JkLnR5cGUgPSBcInBhc3N3b3JkXCJcbiAgICB9XG59KSJdLCJuYW1lcyI6W10sInNvdXJjZVJvb3QiOiIifQ==\n//# sourceURL=webpack-internal:///./src/signup.js\n");
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module can't be inlined because the eval-source-map devtool is used.
+/******/ 	var __webpack_exports__ = {};
+/******/ 	__webpack_modules__["./src/signup.js"]();
+/******/ 	
+/******/ })()
+;
--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,24 @@
 const path = require('path')
 
-module.exports = {
-mode: 'development',
-  entry: ['./src/chat.js', './src/interface.js', './src/index.js'],
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js'
+module.exports = [
+  {
+    mode: 'development',
+      entry: ['./src/chat.js', './src/interface.js', './src/index.js'],
+      output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js' 
+      },
+      devtool: 'eval-source-map',
+      watch: true,
   },
-  devtool: 'eval-source-map',
-  watch: true,
-}
+  {
+    mode: 'development',
+      entry: ['./src/signup.js'],
+      output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundleTwo.js' 
+      },
+      devtool: 'eval-source-map',
+      watch: true,
+    }
+] 
